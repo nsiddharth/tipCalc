00115b62475c5c38280dfbcf4b863b38c9bedabc
nsiddharth <n.siddharth@gail.com>
2014-09-09 00:08:27 -0700

    initial commit

--- tipCalc/ViewController.swift
+++ tipCalc/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var tipLabel : UILabel
+    
+    @IBOutlet var totalLabel : UILabel
+    
+    
+    @IBOutlet var billField : UITextField
+    
+    @IBOutlet var tipSlider : UISlider
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        billField.keyboardType = UIKeyboardType.DecimalPad
+        
+        tipLabel.text="Test text"
+        totalLabel.text=" "
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +33,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender : AnyObject) {
+        updateValues()
+        view.endEditing(true)
+    }
+    
+    @IBAction func onSlide(sender : AnyObject) {
+        
+        var tipVal:Float = tipSlider.value
+        
+        var percentage = tipVal*100
+        
+        tipLabel.text="\(percentage)"
+        
+        var bill = NSString (string:billField.text).floatValue
 
+        
+        var total = bill + percentage*bill/100
+        
+        
+        totalLabel.text="\(total)"
+        
+    }
+
+    func updateValues(){
+    
+        
+        
+        
+        
+    }
+    
 }
 
